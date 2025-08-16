d654a8eb3c1e84c938c605394ef4f439f8f06371
Milnandy <arrafii1910@gmail.com>
2025-08-16 08:51:43 +0700

    js(feat): update

--- random-number-guessing.js
+++ random-number-guessing.js
@@ -13,8 +13,10 @@ while(running){
     guess = window.prompt(`Guess a number between ${minNumber} and ${maxNumber}:`);
     guess = Number(guess);
 
+
+
     if (isNaN(guess)) {
-        windonw.alert("Please enter a valid number.");
+        window.alert("Please enter a valid number.");
     }
     else if(guess < minNumber || guess > maxNumber) {
         window.alert(`Please enter a number between ${minNumber} and ${maxNumber}.`);
